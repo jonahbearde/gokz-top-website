17647a50157f13a9517600f08fd4439799d89b66
jonahbearde <bearinparadox@gmail.com>
2024-05-20 19:06:39 +0800

    feat: search by name/steamid

--- app.vue
+++ app.vue
@@ -1,8 +1,15 @@
 <script setup lang="ts">
 import type { Player } from "./types/player"
 import { useStorage } from "@vueuse/core"
+import { debounce } from "radash"
 
 type Mode = "kz_timer" | "kz_simple" | "kz_vanilla"
+
+interface SearchResult {
+  name: string
+  steamid: string
+  avatar_hash: string
+}
 
 useHead({
   title: "GOKZ TOP",
@@ -19,14 +26,18 @@ const loading = ref(false)
 const players = ref<Player[]>([])
 const me = ref<Player>()
 
-const end = ref()
-
-const steamId = useStorage("steamid", () => "")
+const searchQuery = useStorage("search", () => "")
+const searchResults = ref<SearchResult[]>([])
+const search = debounce({ delay: 300 }, searchPlayer)
 
 getRanking()
 
 watch([mode, offset, limit], async () => {
   await getRanking()
+})
+
+watch(searchQuery, (searchQuery) => {
+  search(searchQuery)
 })
 
 async function getRanking() {
@@ -44,12 +55,14 @@ async function getRanking() {
   }
 }
 
-async function getPlayerRank() {
+async function goToPlayer(steamId: string) {
   loading.value = true
+  searchQuery.value = ''
+  searchResults.value = []
   try {
-    if (steamId.value) {
+    if (steamId) {
       const response = await fetch(
-        `${apiBase}/leaderboard/${steamId.value}?mode=${mode.value}`
+        `${apiBase}/leaderboard/${steamId}?mode=${mode.value}`
       )
 
       me.value = await response.json()
@@ -63,6 +76,22 @@ async function getPlayerRank() {
     console.log(error)
   } finally {
     loading.value = false
+  }
+}
+
+async function searchPlayer(searchQuery: string) {
+  try {
+    if (searchQuery === "") {
+      searchResults.value = []
+    } else {
+      const response = await fetch(
+        `${apiBase}/leaderboard/search/${searchQuery}`
+      )
+      searchResults.value = await response.json()
+    }
+  } catch (error) {
+    searchResults.value = []
+    console.log(error)
   }
 }
 
@@ -114,18 +143,31 @@ function nextPage() {
         </div>
       </div>
 
-      <div class="flex items-center gap-2">
+      <div class="relative flex items-center gap-2">
+        <p class="text-lg">Search</p>
         <input
           type="text"
-          v-model="steamId"
-          placeholder="Steam ID"
+          v-model="searchQuery"
+          placeholder="Name, Steam ID"
           class="border border-gray-400 p-1"
         />
+
         <div
-          class="cursor-pointer hover:bg-gray-200 px-2 py-1 whitespace-nowrap"
-          @click="getPlayerRank"
+          v-if="searchResults.length > 0"
+          class="absolute top-10 p-1 z-20 bg-gray-200 border border-gray-400"
         >
-          Find Your Place
+          <div
+            v-for="result in searchResults"
+            :key="result.steamid"
+            class="mt-1 flex items-center hover:bg-gray-300 cursor-pointer"
+            @click="goToPlayer(result.steamid)"
+          >
+            <img
+              :src="`https://avatars.cloudflare.steamstatic.com/${result.avatar_hash}_medium.jpg`"
+              class="w-8 h-auto rounded-sm"
+            />
+            <p class="px-2 truncate text-ellipsis">{{ result.name }}</p>
+          </div>
         </div>
       </div>
 
@@ -151,29 +193,40 @@ function nextPage() {
       </div>
     </div>
 
-    
     <div class="overflow-x-auto">
-      <table class="mt-4  w-full border border-gray-300 text-center">
+      <table class="mt-4 w-full border border-gray-300 text-center">
         <thead class="sticky top-0">
           <tr>
             <th rowspan="2" class="bg-gray-200">Rank</th>
+
             <th rowspan="2" class="bg-gray-300">Player</th>
+
             <th rowspan="2" class="bg-gray-200">Rating</th>
+
             <th rowspan="2" class="bg-gray-300">Top%</th>
+
             <th rowspan="2" class="bg-gray-200">Level</th>
+
             <th rowspan="2" class="bg-gray-300">Points</th>
+
             <th rowspan="2" class="bg-gray-200 whitespace-nowrap">
               Maps Finished
             </th>
+
             <th colspan="2" class="text-center bg-gray-300">WRs</th>
+
             <th colspan="2" class="bg-gray-200">Records</th>
+
             <th colspan="3" class="bg-gray-300 whitespace-nowrap">
               Hard Maps Finished
             </th>
+
             <th colspan="3" class="bg-gray-200">Avg Points</th>
+
             <th rowspan="2" class="bg-gray-300 whitespace-nowrap">
               Most Played Server
             </th>
+
             <th rowspan="2" class="bg-gray-200 whitespace-nowrap">
               Updated On
             </th>
@@ -181,20 +234,17 @@ function nextPage() {
           <tr>
             <th class="bg-gray-300">PRO</th>
             <th class="bg-gray-300">TP</th>
+
             <th class="bg-gray-200">900+</th>
             <th class="bg-gray-200">800+</th>
-            <!-- <th class="bg-gray-300 whitespace-nowrap">900+(T5-T7)</th>
-            <th class="bg-gray-300 whitespace-nowrap">800+(T5-T7)</th>
-            <th class="bg-gray-300 whitespace-nowrap">PRO(T5-T7)</th> -->
+
             <th class="bg-gray-300">T5</th>
             <th class="bg-gray-300">T6</th>
             <th class="bg-gray-300">T7</th>
+
             <th class="bg-gray-200">TP+PRO</th>
             <th class="bg-gray-200">PRO</th>
             <th class="bg-gray-200">TP</th>
-            <!-- <th class="bg-gray-300">T5</th>
-            <th class="bg-gray-300">T6</th>
-            <th class="bg-gray-300">T7</th> -->
           </tr>
         </thead>
         <tbody v-if="loading">
@@ -210,6 +260,7 @@ function nextPage() {
             class="border-y hover:bg-gray-200"
           >
             <td>{{ player.rank }}</td>
+
             <td class="py-1 cursor-pointer">
               <div
                 class="flex items-center gap-2"
@@ -224,38 +275,43 @@ function nextPage() {
                 </p>
               </div>
             </td>
+
             <td>{{ player.pts_skill }}</td>
+
             <td>{{ player.percentage }}</td>
+
             <td>
               <img
                 :src="`/ranks/${player.rank_name.toLowerCase()}.png`"
                 class="w-20 h-auto inline"
               />
             </td>
+
             <td>{{ player.total_points.toLocaleString() }}</td>
+
             <td>{{ player.count }}</td>
+
             <td>{{ player.count_p1000_pro }}</td>
             <td>{{ player.count_p1000_tp }}</td>
+
             <td>{{ player.count_p900 }}</td>
             <td>{{ player.count_p800 }}</td>
-            <!-- <td>{{ player.count_t567_p800 }}</td>
-            <td>{{ player.count_t567_p900 }}</td>
-            <td>{{ player.count_t567_pro }}</td> -->
+
             <td>{{ player.count_t5 }}</td>
             <td>{{ player.count_t6 }}</td>
             <td>{{ player.count_t7 }}</td>
+
             <td>{{ player.pts_avg }}</td>
             <td>{{ player.pts_avg_pro }}</td>
             <td>{{ player.pts_avg_tp }}</td>
+
             <td class="max-w-32 truncate text-ellipsis whitespace-nowrap">
               {{ player.most_played_server }}
             </td>
+
             <td class="max-w-32 truncate text-ellipsis whitespace-nowrap">
               {{ toLocal(player.updated_on) }}
             </td>
-            <!-- <td>{{ player.pts_avg_t5 }}</td>
-            <td>{{ player.pts_avg_t6 }}</td>
-            <td>{{ player.pts_avg_t7 }}</td> -->
           </tr>
         </tbody>
       </table>
